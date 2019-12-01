d1c5604e79e77cc24084b206d41924b0d8162a4e
Andrew Hove <andrewhove@Andrews-MacBook-Pro.local>
2019-12-01 16:55:37 -0600

    Updates

---
+++ precache-manifest.fe26619c462e66c8e7393b3af99e6080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f5f14bcb767594adf689eaa401df5c",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "e2152b270950c37dd6e4",
+    "url": "/MonstersRolodex/static/css/main.14ddfb83.chunk.css"
+  },
+  {
+    "revision": "59c13745251206e2f1b0",
+    "url": "/MonstersRolodex/static/js/2.0729de66.chunk.js"
+  },
+  {
+    "revision": "e2152b270950c37dd6e4",
+    "url": "/MonstersRolodex/static/js/main.ed5f14c6.chunk.js"
+  },
+  {
+    "revision": "87c8ad3ad93ea3b2966e",
+    "url": "/MonstersRolodex/static/js/runtime-main.b6e90933.js"
+  }
+]);
