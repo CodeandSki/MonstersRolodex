b97b7c7b41a2f98bb6adf7cdc573446df9ce7ca5
Andrew Hove <andrewhove@Andrews-MacBook-Pro.local>
2019-12-02 03:11:48 -0600

    updated picture size

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.styles.css';
 export const Card = props => (
     <div className="card-container">
         {console.log(props, 'Child')}
-        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}/>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=240x240`}/>
         <h1>{props.monster.name}</h1>
     </div>
 );
